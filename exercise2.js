459a1926ebab93725b82e489c31f68730b4a1a19
Nicolai <nicolai@mmg.io>
2017-02-21 14:45:58 +0000

    Eloquent JavaScript Objects & Arrays exercises

---
+++ exercise2.js
@@ -0,0 +1,22 @@
+function reverseArray (array) {
+ var array1 = [];
+   for (var i = array.length - 1; i >= 0; i--)
+  array1.push(array[i]);
+   return array1;
+}
+
+function reverseArrayInPlace (array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var array2 = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = array2;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
